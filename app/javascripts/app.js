09c47d1ebb2fe2ade6e6609d66a5efdb56ecf038
shankermjj <shanker.m955@gmail.com>
2019-03-06 14:20:00 +0530

    First commit

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,142 +1,102 @@
 // Import the page's CSS. Webpack will know what to do with it.
-import "../stylesheets/app.css";
+// import "../stylesheets/landing-page.css";
 
 // Import libraries we need.
-import {  default as Web3 } from 'web3';
-import {  default as contract } from 'truffle-contract';
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import si_artifacts from '../../build/contracts/SI.json'
+// import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
 
-// Conference is our usable abstraction, which we'll use through the code below.
-var SI = contract(si_artifacts);
+// MetaCoin is our usable abstraction, which we'll use through the code below.
+// var MetaCoin = contract(metacoin_artifacts);
 
-var accounts;
-var sim;
+// The following code is simple to show off interacting with your contracts.
+// As your needs grow you will likely need to change its form and structure.
+// For application bootstrapping, check out window.addEventListener below.
+// var accounts;
+// var account;
 
-function getBalance1(address) {
-    return web3.fromWei(web3.eth.getBalance(address).toNumber(), 'ether');
-}
+// window.App = {
+//   start: function() {
+//     var self = this;
 
+//     // Bootstrap the MetaCoin abstraction for Use.
+//     MetaCoin.setProvider(web3.currentProvider);
 
+//     // Get the initial account balance so it can be displayed.
+//     web3.eth.getAccounts(function(err, accs) {
+//       if (err != null) {
+//         alert("There was an error fetching your accounts.");
+//         return;
+//       }
 
-function getBalance (address) {
-  return new Promise (function (resolve, reject) {
-    web3.eth.getBalance(address, function (error, result) {
-      if (error) { reject(error); } 
-	else { resolve(result);   }
-  })
-})
-}
+//       if (accs.length == 0) {
+//         alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+//         return;
+//       }
 
-window.App = { //where to close
-    start: function() {
-        var self = this;
+//       accounts = accs;
+//       account = accounts[0];
 
-        web3.eth.getAccounts(function(err, accs) {
-            if (err != null) {
-                alert("There was an error fetching your accounts.");
-                return;
-            }
+//       self.refreshBalance();
+//     });
+//   },
 
-            if (accs.length == 0) {
-                alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-                return;
-            }
- accounts = accs;
-//$("#tentantAddress").html(getBalance(accounts[0])); //prints balance
+//   setStatus: function(message) {
+//     var status = document.getElementById("status");
+//     status.innerHTML = message;
+//   },
 
-$("#tentantAddress").text(web3.eth.accounts[0]); //prints account address
-            //console.log(accounts);
-                 
-            self.initializeConference();
-        });
-    },
+//   refreshBalance: function() {
+//     var self = this;
 
-    initializeConference: function() {
-        var self = this;
-	
-        SI.deployed().then(function(instance) {
-            sim = instance;
-            $("#confAddress").html(sim.address);
+//     var meta;
+//     MetaCoin.deployed().then(function(instance) {
+//       meta = instance;
+//       return meta.getBalance.call(account, {from: account});
+//     }).then(function(value) {
+//       var balance_element = document.getElementById("balance");
+//       balance_element.innerHTML = value.valueOf();
+//     }).catch(function(e) {
+//       console.log(e);
+//       self.setStatus("Error getting balance; see log.");
+//     });
+//   },
 
-            self.checkValues();
-        }).catch(function(e) {
-            console.log(e);
-        });
+//   sendCoin: function() {
+//     var self = this;
 
-    },
+//     var amount = parseInt(document.getElementById("amount").value);
+//     var receiver = document.getElementById("receiver").value;
 
+//     this.setStatus("Initiating transaction... (please wait)");
 
-
-
-checkValues: function() {
-
-        SI.deployed().then(function(instance) {
-           sim = instance;
-	    console.log(sim);	
-            sim.tenant.call().then
-
-	( function(tent) { console.log(tent); $("input#tentantAddress").val(tent);  }). //})
-	
-	catch(function(e) { console.log(e); });
-	});
-   },
-
-Calculate: function(pr,ti,ra) 
-{
-        var simp;
-        SI.deployed().
-
-	then( function(instance) { simp = instance; simp.calculate(pr,ti,ra,{from: accounts[0],gas:3000000}). //(}
-
-then( function() { console.log(pr); console.log(ti);console.log(ra);console.log( JSON.stringify(SI) ); return simp.Principal.call(); }).
-	then( function(Principal) { console.log(Principal);   return simp.Time.call();  }). //console.log(Time); 
-	then( function(Time) { console.log(Time); return simp.Rate.call();}).
-	then( function(Rate) { console.log(Rate); return simp.Interest.call(); }).
-	then( function(Interest) { console.log(Interest);  $("#i").html(Interest.toNumber()); });  }). 
-        catch(function(e) { console.log(e); });
-    	
-//}); //})
-}
-
- 
-
-
-
-
-};//loop for main
-
- 
-
-
+//     var meta;
+//     MetaCoin.deployed().then(function(instance) {
+//       meta = instance;
+//       return meta.sendCoin(receiver, amount, {from: account});
+//     }).then(function() {
+//       self.setStatus("Transaction complete!");
+//       self.refreshBalance();
+//     }).catch(function(e) {
+//       console.log(e);
+//       self.setStatus("Error sending coin; see log.");
+//     });
+//   }
+// };
 
 window.addEventListener('load', function() {
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-        console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-        // Use Mist/MetaMask's provider
-        window.web3 = new Web3(web3.currentProvider);
-    } else {
-        console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-        window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-    }
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.warn("No web3 detected. Falling back to http://localhost:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
 
-    SI.setProvider(web3.currentProvider);
-    App.start();
-
-    // Wire up the UI elements
-
-
-
-    $("#caluclate").click(function() {
-        var pr = $("#p").val();
-	var ra = $("#r").val();
-	var ti = $("#t").val();
-        App.Calculate(pr,ti,ra);
-    });
-
-    
-
+  // App.start();
 });
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
-var Simpleinterest = artifacts.require("./SI.sol");
+var CrowdBank = artifacts.require("./CrowdBank.sol");
+var Mortgage = artifacts.require("./Mortgage.sol");
 module.exports = function(deployer) {
-  deployer.deploy(Simpleinterest);
+  deployer.deploy(CrowdBank);
+  deployer.deploy(Mortgage);
 };
-
